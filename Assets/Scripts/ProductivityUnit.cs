62564c168ba911e34fdb575872a56c867aa9de26
Lotus <114036124+CodeLotusGX7@users.noreply.github.com>
2023-08-27 03:43:38 -0400

    add productivity unit and functionality to it

--- Assets/Scripts/ProductivityUnit.cs
+++ Assets/Scripts/ProductivityUnit.cs
@@ -11,6 +11,35 @@ public class ProductivityUnit : Unit // replaced MonoBehaviour with Unit
     // <-- new variables here :D -->
     protected override void BuildingInRange()
     {
-        
+        if (m_CurrentPile == null)
+        {
+            ResourcePile pile = m_Target as ResourcePile;
+
+            if (pile != null)
+            {
+                m_CurrentPile = pile;
+                m_CurrentPile.ProductionSpeed *= ProductivityMultiplier;
+            }
+        }
+    }
+
+    void ResetProductivity()
+    {
+        if (m_CurrentPile != null)
+        {
+            m_CurrentPile.ProductionSpeed /= ProductivityMultiplier;
+            m_CurrentPile = null;
+        }
+    }
+    public override void GoTo(Building target) // for targeting buildings
+    {
+        ResetProductivity(); // call method to put pile rate to normal
+        base.GoTo(target); // run method from base class
+    }
+
+    public override void GoTo(Vector3 position) // for targeting random location on map
+    {
+        ResetProductivity();
+        base.GoTo(position);
     }
 }
